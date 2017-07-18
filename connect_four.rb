484bb67bb64de65ac7597ba36f140f9daf611465
Cody Buffaloe <codylbuffaloe@gmail.com>
2017-07-17 17:15:41 -0700

    Began rspec for connect four board

---
+++ connect_four.rb
@@ -0,0 +1,6 @@
+module Connect_Four
+end
+require_relative "./lib/board.rb"
+require_relative "./lib/core_extensions.rb"
+require_relative "./lib/game.rb"
+require_relative "./lib/player.rb"
---
+++ spec/board_spec.rb
@@ -0,0 +1,6 @@
+require "spec_helper"
+
+module Connect_four
+  describe Board do
+  end
+end
