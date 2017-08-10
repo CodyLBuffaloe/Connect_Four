593c16a7a9e19e3023023fb9b83e1b77dcc45ac2
Cody Buffaloe <codylbuffaloe@gmail.com>
2017-08-10 14:29:06 -0700

    Fixed regex so it actually matches with text in examplegame

--- example_game.rb
+++ example_game.rb
@@ -5,7 +5,7 @@ p1_name = gets.chomp
 player1 = Connect_Four::Player.new({name: p1_name, color: :yellow})
 puts "If there is another player, supply their name, otherwise the computer will destroy you\n ...in Connect Four. They will play as red."
 p2_name = gets.chomp
-if p2_name == /^[A-z]+$/
+if p2_name =~ /^[A-z]+$/
   player2 = Connect_Four::Player.new({name: p2_name, color: :red})
 else
   puts "Hey, that isn't a name!"
